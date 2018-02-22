c1b4013fee8ca3928724f426c7827b74ebcc5f7b
franksalas <frank.salas@gmail.com>
2018-02-21 23:58:20 -0600

    added render_math, salasdev theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ SITENAME = 'salas'
 SITEURL = ''
 
 PATH = 'content'
-
+STATIC_PATHS = ['images']
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'en'
@@ -26,10 +26,33 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+          ('linkedin', 'https://www.linkedin.com/in/frank-salas/'),
+          ('github', 'http://github.com/franksalas'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+### Themes ###
+ARTICLE_URL = '{slug}/'
+ARTICLE_SAVE_AS = '{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+# THEME and variables
+THEME = './themes/salasdev'
+
+SHOW_ARCHIVES = False
+SHOW_TAGS = False
+
+
+
+
+
+#### Plugins ####
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup','render_math']
